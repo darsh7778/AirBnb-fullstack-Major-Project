69dbcc11add07a2450f6b4f020092e85bbb36752
Darshil Soni <sonidarsh2005@gmail.com>
2025-08-18 18:28:28 +0530

    two files modified app.js and express.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const mongoose = require("mongoose");
 const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const ExpressError = require("./utils/ExpressError.js");
+const ExpressError = require("./utils/expressError.js");
 const session = require("express-session");
 const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
@@ -49,7 +49,7 @@ const store = MongoStore.create({
   touchAfter: 24 * 3600,
 });
 
-store.on("error", () => {
+store.on("error", (err) => {
   console.log("ERROR IN MONGO SESSION STORE", err);
 });
 
@@ -86,9 +86,9 @@ app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
 
-// app.all("*", (req, res, next) => {
-//   next(new ExpressError(404, "Page not found!"));
-// });
+app.use((req, res, next) => {
+  next(new ExpressError(404, "Page not found!"));
+});
 
 app.use((err, req, res, next) => {
   console.error(err);
